b87427db7968a82a3bad7f30f5e2d320eba28dad
FAVOURCHINA <fc032990@gmail.com>
2023-02-16 16:18:36 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include<unistd.h>
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline,to standard error.
- * Return:Always 1.
+ * Return:Always 1 (Success)
  */
 int main(void)
 {
