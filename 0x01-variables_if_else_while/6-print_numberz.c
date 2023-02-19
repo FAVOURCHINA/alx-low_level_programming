c4072fbb5159283d54a26654c98efd1d965ddaf9
FAVOURCHINA <fc032990@gmail.com>
2023-02-19 14:18:42 -0800

    6-print_numberz.c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	putchar(a + '0');
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
