ebcfdfe3a6b3985b88c0a51df0f61cd96119fdd2
FAVOURCHINA <fc032990@gmail.com>
2023-02-19 04:34:17 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,17 +2,16 @@
 /**
  * main - prints alphebet in lower case.
  *
- * Retur: always 0 ( Success)
+ * Return: always 0 ( Success)
  */
 int main(void)
 {
 	char i;
-	for (("i = 'a' ; i <= 'z' ; i++")
-		
+
+	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
-	
 		putchar(i);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
