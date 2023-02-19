371c5361e3e60fbe13281212b67eceac6522f96f
FAVOURCHINA <fc032990@gmail.com>
2023-02-19 13:19:09 -0800

    4-print_alphabt.c task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 'a';
+	while (i <= 'z')
+
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
