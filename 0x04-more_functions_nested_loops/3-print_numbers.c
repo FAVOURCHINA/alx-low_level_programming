0b54f6db94e4878f06ad940aba4861da0f5ba543
FAVOURCHINA <fc032990@gmail.com>
2023-02-23 14:20:29 -0800

    3-print_numbers.c task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for a digit (0 thriugh 9)
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int C;
+
+	for (C = 48; C < 58; C++)
+	{
+	_putchar(C);
+	}
+	_putchar('\n');
+}
