a4f4775d873a797d3c9cdf24b2b15dd740d1ebde
FAVOURCHINA <fc032990@gmail.com>
2023-02-23 14:28:45 -0800

    4-print_most_numbers.c task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - checks fir checks fir a digit(0 through 9)
+ *
+ * Return: Alwasy 0.
+ */
+void print_most_nunbers(void)
+{
+int C;
+for (C = 48; C < 58; C++)
+{
+if (C != 50)
+{
+if (C != 52)
+{
+_putchar(C);
+}
+}
+}
+_putchar('\n');
+}
