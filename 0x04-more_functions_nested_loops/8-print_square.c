a5af18bf2ec69bef5dfdef1a9dc5cc1d7b6a030f
FAVOURCHINA <fc032990@gmail.com>
2023-02-24 03:54:40 -0800

    8-main.c task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,22 +6,21 @@
  */
 void print_square(int size)
 {
-if (size <= 0)
-{
-_putchar('\n');
-{
-else
-}
-int i, j;
-
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
+	int i,j;
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
 }
 
