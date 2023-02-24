b3c7e59f9567e4f7ab0a4b8a9f80b51dde49ff73
FAVOURCHINA <fc032990@gmail.com>
2023-02-23 16:01:14 -0800

    100-prime_factor.c task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -17,6 +17,18 @@ max = -1;
 while (n % 2 == 0)
 {
 max = 2;
+n /= 2;
+}
+
+for (i = 3; i <= sqrt(n); i = i + 2)
+{
+while (n % i == 0)
+{
+max = i;
+n = n / i;
+}
+}
+if (n > 2)
 max = n;
 
 printf("%ld\n", max);
